53bad91f7c3bb2d74ed0da28155153ad9a98a8a1
DevendraPPatil <devendra.patil@tekditechnologies.com>
2025-01-15 12:31:27 +0530

    Feat:1.Added the language complexity for the odiya langauge

--- src/main/java/org/ekstep/language/measures/Application.java
+++ src/main/java/org/ekstep/language/measures/Application.java
@@ -1,5 +1,4 @@
 package org.ekstep.language.measures;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
@@ -12,5 +11,6 @@ public class Application {
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
+        System.out.println("Application is start");
     }
 }
